b3e134b98ef1660f27ca7919da59fa1b9fa6da4b
Sorayya <sorayya Niazi>
2015-11-10 16:58:47 -0700

    Android_Part3

---
+++ GreedySnake/app/src/main/java/com/example/sorayyaniazi/greedysnake/SettingActivity.java
@@ -0,0 +1,40 @@
+package com.example.sorayyaniazi.greedysnake;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by sorayyaniazi on 11/10/15.
+ */
+
+
+public class SettingActivity extends Activity {
+
+// component Reference Variabes.
+
+
+
+//class variable.
+
+
+    @Override
+    protected void onCreate (Bundle savedState){
+
+        super.onCreate(savedState);
+
+//sets the view for class to specify xml file.(Resources.Layout.WelcomeActivity)
+        setContentView(R.layout.setting_activity);
+
+        //Assign component Reference variable.
+        //Example: textBox= (TextView)findViewById(R.id.usernameTxt);
+
+    }
+//Action functions go here, click button,etc.
+   /* public void enterClicked(View view){
+
+        //get username from textBox object
+        username= textBox.getText().toString();
+        System.out.println(username);
+    }
+*/
+}
